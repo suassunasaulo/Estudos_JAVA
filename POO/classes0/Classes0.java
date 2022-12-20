06a5bed8b11f81d16bc3c1cfcde55761acc684c0
Saulo Suassuna <saulosuassuna123@gmail.com>
2022-12-19 21:14:18 -0300

    Exercícios desenvolvidos por meio de POO.

---
+++ POO/classes0/Classes0.java
@@ -0,0 +1,22 @@
+
+// Exercício desenvolvido por meio de POO.
+
+package classes0;
+import java.util.Scanner;
+
+public class Classes0 {
+
+    public static void main(String[] args) {
+        Pessoas pessoa1 = new Pessoas();
+        
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Digite seu peso: ");
+        pessoa1.peso = ler.nextFloat();
+        System.out.println("Digite sua altura: ");
+        pessoa1.altura = ler.nextFloat();
+        
+        System.out.println("SEU IMC É: " + pessoa1.calcularIMC());
+    }
+    
+}
---
+++ POO/classes0/Pessoas.java
@@ -0,0 +1,14 @@
+
+package classes0;
+
+public class Pessoas {
+    
+    float peso, altura;
+    
+    public float calcularIMC(){
+       float imc = peso/(altura*altura);
+       return imc;
+        
+    }
+    
+}
