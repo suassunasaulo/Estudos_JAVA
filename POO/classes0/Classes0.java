6993fb393a97a8b376c27c7c0e7ffcdde76c4606
Saulo Suassuna <saulosuassuna123@gmail.com>
2022-12-20 23:22:28 -0300

    Exercícios desenvolvidos usando POO com os métodos GET e SET.

--- POO/classes0/Classes0.java
+++ POO/classes0/Classes0.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
 public class Classes0 {
 
     public static void main(String[] args) {
-        Pessoas pessoa1 = new Pessoas();
-        
+
+        Pessoas pessoa1 = new Pessoas();        
         Scanner ler = new Scanner(System.in);
         
         System.out.println("Digite seu peso: ");
-        pessoa1.peso = ler.nextFloat();
+        pessoa1.setPeso(ler.nextFloat());
         System.out.println("Digite sua altura: ");
-        pessoa1.altura = ler.nextFloat();
+        pessoa1.setAltura(ler.nextFloat());
         
         System.out.println("SEU IMC É: " + pessoa1.calcularIMC());
     }
--- POO/classes0/Pessoas.java
+++ POO/classes0/Pessoas.java
@@ -3,12 +3,34 @@ package classes0;
 
 public class Pessoas {
     
-    float peso, altura;
+  private  float peso;
+  private float altura;
     
     public float calcularIMC(){
        float imc = peso/(altura*altura);
        return imc;
         
     }
+
+    public void setPeso(float peso){
+    this.peso = peso;
+    }
+
+    public float getPeso(){
+        return peso;
+    }
+     
+    public void setAltura(float altura){
+    this.altura = altura;
+    } 
+    
+    public float getaltura(){
+        return altura;
+    }
+    
+   
     
 }
+
+    
+
